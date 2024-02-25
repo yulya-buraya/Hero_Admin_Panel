d1a59217a598a09c53ee99d036f1e7faeae2dee5
buraya.yuliya <burayayulya374@gmail.com>
2024-02-25 16:29:37 +0300

    fetchFilters

--- src/components/heroesFilters/FiltersSlice.js
+++ src/components/heroesFilters/FiltersSlice.js
@@ -1,10 +1,19 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { useHttp } from "../../hooks/http.hook";
 
 const initialState = {
     filters: [],
     filtersLoadingStatus: 'idle',
     activeFilter: 'all'
 }
+
+export const fetchFilters = createAsyncThunk(
+  "filters/fetchFilters",
+  async() =>{
+    const {request} = useHttp();
+    return await request("http://localhost:3001/filters");
+  }
+)
 
 export const filtersSlice = createSlice({
     name: "filters",
@@ -23,6 +32,19 @@ export const filtersSlice = createSlice({
       activeFilterChanged: (state, action) => {
         state.activeFilter = action.payload;
       }
+    },
+    extraReducers:(builder) =>{
+      builder
+      .addCase(fetchFilters.pending, state => {
+        state.filtersLoadingStatus = "loading"
+    })
+      .addCase(fetchFilters.fulfilled, (state,action) => {
+        state.filtersLoadingStatus = "idle";
+        state.filters = action.payload;
+      })
+      .addCase(fetchFilters.rejected, state => {
+        state.filtersLoadingStatus = "error";
+      })
     }
 });
 
